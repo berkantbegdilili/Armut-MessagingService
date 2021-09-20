785fac65f80ebb9f79de591075cc8a6c76c37b14
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2021-09-21 02:44:04 +0300

    user model have been append

---
+++ src/models/collections/user.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
+const mainSchema = mongoose.Schema({
+    username: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+    conversations: [ { type: String, ref: 'Conversation'} ]
+}, {
+    timestamp: true,
+    versionKey: false,
+    id: false,
+    toJSON:{ 
+        virtuals: true
+    },
+    toObject:{
+        virtuals: true
+    }
+});
+
+mainSchema.pre('save', async function save(next) {
+    if (!this.isModified('password')) return next();
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+      return next();
+    } catch (err) {
+      return next(err);
+    }
+});
+
+mainSchema.methods.validatePassword = async function validatePassword(data) {
+    return bcrypt.compare(data, this.password);
+};
+
+const User = mongoose.model("User", mainSchema);
+
+module.exports = {
+    userSchema: mainSchema,
+    User
+};
