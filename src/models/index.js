19a7616ea8f6dc7d514577cf6aeefca43b500a8e
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2021-09-21 22:41:58 +0300

    redis client have been append

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Redis = require("ioredis");
 
 const db = {};
 
@@ -6,16 +7,30 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useUnifiedTopology: true
 });
 
 db.mongoose = mongoose;
+
+const redisClient = new Redis(
+    { 
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWORD,
+        retryStrategy(times) {
+            const delay = Math.min(times * 50, 2000);
+            return delay;
+        }
+    }
+);
+
+db.redisClient = redisClient;
 
 // Collections
 db.Users = require('./collections/user.model').User;
 db.Conversations = require('./collections/conversation.model').Conversation;
 db.Messages = require('./collections/message.model').Message;
 db.Blockeds = require('./collections/blocked.model').Blocked;
+db.LoginLogs = require('./collections/login-log.model').LoginLog;
 
 module.exports = db;
