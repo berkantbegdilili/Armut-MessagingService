72a18e664a0fd8c747cf0e24ec5eee83857822e4
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2021-09-22 00:50:48 +0300

    redis client have been deleted

--- src/models/index.js
+++ src/models/index.js
@@ -12,20 +12,6 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 db.mongoose = mongoose;
 
-const redisClient = new Redis(
-    { 
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-        password: process.env.REDIS_PASSWORD,
-        retryStrategy(times) {
-            const delay = Math.min(times * 50, 2000);
-            return delay;
-        }
-    }
-);
-
-db.redisClient = redisClient;
-
 // Collections
 db.Users = require('./collections/user.model').User;
 db.Conversations = require('./collections/conversation.model').Conversation;
