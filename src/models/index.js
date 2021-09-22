34908bb72f85c7071f66b103fe46be93c283d9c4
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2021-09-22 08:22:43 +0300

    redis have been append

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const Redis = require("ioredis");
+var session = require("express-session");
+var RedisStore = require('connect-redis')(session);
+const { RateLimiterMemory, RateLimiterRedis } = require('rate-limiter-flexible');
 
 const db = {};
 
+// Mongodb
 mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.DATABASE_URL, {
@@ -11,6 +15,49 @@ mongoose.connect(process.env.DATABASE_URL, {
 });
 
 db.mongoose = mongoose;
+
+// Redis
+const redisClient = new Redis(
+    { 
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWORD,
+        retryStrategy(times) {
+            const delay = Math.min(times * 50, 2000);
+            return delay;
+        }
+    }
+);
+
+db.redisClient = redisClient;
+
+db.redisSessionStore = new RedisStore({
+    prefix: 'session:', 
+    client: redisClient,
+    ttl: 60 * 60 * 1000 
+});
+
+const rateLimiterMemory = new RateLimiterMemory({
+    points: 20,
+    duration: 60
+});
+
+db.rateLimiterRedis = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: 'rateLimiter',
+    points: 100, 
+    duration: 60,
+    inmemoryBlockOnConsumed: 101,
+    inmemoryBlockDuration: 60,
+    insuranceLimiter: rateLimiterMemory 
+});
+
+db.limiterConsecutiveOutOfLimits = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: 'consecutiveOutOfLimits',
+    points: 99999, 
+    duration: 0
+});
 
 // Collections
 db.Users = require('./collections/user.model').User;
